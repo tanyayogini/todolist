4ee86a0b430c065b9d2dafb585bc30fe5c1cdeef
tatianakushnareva <tvkushnareva@yandex.ru>
2023-07-20 20:10:20 +0500

    add board to admin

--- goals/admin.py
+++ goals/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 
-from goals.models import GoalCategory, Goal, GoalComment
+from goals.models import GoalCategory, Goal, GoalComment, Board
 
 
 class GoalCategoryAdmin(admin.ModelAdmin):
-    list_display = ("title", "user", "created", "updated", "is_deleted")
+    list_display = ("title", "user", "created", "updated", "is_deleted", "board")
     search_fields = ("title", "user")
 
 
@@ -18,6 +18,11 @@ class GoalCommentAdmin(admin.ModelAdmin):
     search_fields = ("text", "user")
 
 
+class BoardAdmin(admin.ModelAdmin):
+    list_display = ("title", "created", "updated")
+
+
 admin.site.register(GoalCategory, GoalCategoryAdmin)
 admin.site.register(Goal, GoalAdmin)
 admin.site.register(GoalComment, GoalCommentAdmin)
+admin.site.register(Board, BoardAdmin)
